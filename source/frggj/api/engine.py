200081664794747402ff015380d834a6d0eee8c9
Marco Romeo <mromeo@inbibo.co.uk>
2026-01-30 18:04:26 +0100

    first engine run

--- source/frggj/api/engine.py
+++ source/frggj/api/engine.py
@@ -1,10 +1,12 @@
-# game
+# engine
 import pygame as pg
 import numpy as np
-from groucho.canvas import GCanvas
-from groucho.scene import GScene
+from frggj.api.game import GGame
+from frggj.api.canvas import GCanvas
 
-class GGameState(object):
+# from groucho.canvas import GCanvas
+
+class GEngineState(object):
     kIntro = 1
     kMenu = 2
     kEnd = 3
@@ -13,8 +15,8 @@ class GGameState(object):
 
 class GEngine(object):
     def __init__(self, fullscreen=False):
-        self._state = GGameState.kMenu
-        self._game = 
+        self._state = GEngineState.kMenu
+        self._game = GGame()
 
         self._width = 426
         self._height = 240
@@ -24,12 +26,11 @@ class GEngine(object):
         self._font = self._pg.font.SysFont("Arial" , 18 , bold = True)
         self._clock = self._pg.time.Clock()
         if fullscreen:
-            self._screen = self._pg.display.set_mode((self._width * scale_factor, self._height * scale_factor), self._pg.FULLSCREEN)
+            self._screen = self._pg.display.set_mode((self._width * self._scale_factor, self._height * self._scale_factor), self._pg.FULLSCREEN)
         else:
-            self._screen = self._pg.display.set_mode((self._width * scale_factor, self._height * scale_factor))
+            self._screen = self._pg.display.set_mode((self._width * self._scale_factor, self._height * self._scale_factor))
         self._clock = self._pg.time.Clock()
         self._canvas = GCanvas(self._width, self._height, self)
-        self._scene = None
         self._run()
 
     def _run(self):
@@ -42,10 +43,10 @@ class GEngine(object):
                 if event.type == self._pg.KEYDOWN and event.key == self._pg.K_ESCAPE:
                     running = False
             pressed_keys = self._pg.key.get_pressed()
-            if self._state == GState.kMenu:
+            if self._state == GEngineState.kMenu:
                 if pressed_keys[self._pg.K_RETURN]:
-                    self._state = GState.kLevel
-            if self._state == GState.kLevel:
+                    self._state = GEngineState.kLevel
+            if self._state == GEngineState.kLevel:
                 self._run_game()
 
                 """self._init_level()
@@ -74,13 +75,13 @@ class GEngine(object):
                 asset_transform.set_eulers(asset_eulers)
                 self._scene._assets[0].set_transform(asset_transform)"""
                     
-            if self._state == GState.kMenu:
+            if self._state == GEngineState.kMenu:
                 self._run_menu()
-            elif self._state == GState.kLevel:
+            elif self._state == GEngineState.kLevel:
                 self._run_level()
 
             surf = pg.surfarray.make_surface(self._canvas.get_pixels())
-            surf = self._pg.transform.scale(surf, (self._width * scale_factor, self._height * scale_factor))
+            surf = self._pg.transform.scale(surf, (self._width * self._scale_factor, self._height * self._scale_factor))
             self._screen.blit(surf, (0,0))
             fps = str(int(self._clock.get_fps()))
             fps_t = self._font.render(fps , 1, self._pg.Color("RED"))
@@ -88,9 +89,11 @@ class GEngine(object):
             self._pg.display.update()
 
     def run_intro(self):
+        self._canvas.fill(50, 127, 200)
         a = 0
 
     def _run_menu(self):
+        self._canvas.fill(50, 127, 200)
         a = 0
 
     """def _init_game(self):
---
+++ source/frggj/api/game.py
@@ -0,0 +1,8 @@
+# game
+
+
+class GGame(object):
+    def __init__(self):
+        self._current_level = 0
+        self._levels = None
+        self._player = None
